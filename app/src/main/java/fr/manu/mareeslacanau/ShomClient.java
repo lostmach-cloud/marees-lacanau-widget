4a5aad2700b02265b13e330279c8d682cb2057cf
lostmach-cloud <lostmach@gmail.com>
2026-09-06 18:12:12 +0000

    Test connexion Android

--- app/src/main/java/fr/manu/mareeslacanau/ShomClient.java
+++ app/src/main/java/fr/manu/mareeslacanau/ShomClient.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 public class ShomClient {
 
     private static final String SOURCE =
-        "https://www.lacanausurfinfo.com/";
+    "https://www.google.com/";
 
     public static List<Tide> fetch() throws Exception {
 
